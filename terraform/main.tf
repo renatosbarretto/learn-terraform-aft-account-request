0968c9f5c9c1f726c154beb7628d4df41b41935c
renatobarretto <renatobarretto@hotmail.com>
2025-04-05 17:04:00 -0300

    new_account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,32 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+
+module "teste-renato" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "renatosbarretto+aws-teste@gmail.com"
+    AccountName               = "teste-renato"
+    ManagedOrganizationalUnit = "Infrastructure"
+    SSOUserEmail              = "renatosbarretto+aws-testex@gmail.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
