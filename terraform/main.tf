a15c177321024b5b2a0fcea9314c4c376b2bca08
renatobarretto <renatobarretto@hotmail.com>
2025-04-11 21:36:23 -0300

    remove teste account

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,32 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "core-network" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "renatosbarretto+aws-core-network@gmail.com"
+    AccountName               = "core-network"
+    ManagedOrganizationalUnit = "Network"
+    SSOUserEmail              = "renatosbarretto+aws-core-network@gmail.com"
+    SSOUserFirstName          = "core-network"
+    SSOUserLastName           = "AFT"
+  }
+   
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "core-network"
+}
