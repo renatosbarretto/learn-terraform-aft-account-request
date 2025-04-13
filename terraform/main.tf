8b8c76a70cc830bae3fa091c331356998342a0a6
renatobarretto <renatobarretto@hotmail.com>
2025-04-13 11:12:24 -0300

    Comment out sandbox account to remove it

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+/*
 module "sandbox" {
   source = "./modules/aft-account-request"
 
@@ -26,32 +27,4 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
-
-module "core-network" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "renatosbarretto+aws-core-network@gmail.com"
-    AccountName               = "core-network"
-    ManagedOrganizationalUnit = "Network"
-    SSOUserEmail              = "renatosbarretto+aws-core-network@gmail.com"
-    SSOUserFirstName          = "core-network"
-    SSOUserLastName           = "AFT"
-  }
-   
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "core-network"
-}
+*/
---
+++ terraform/network-hub/main.tf
@@ -0,0 +1,32 @@
+module "network_hub" {
+  source = "../../modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "renatosbarretto+aws-network-hub@gmail.com"
+    AccountName               = "network-hub"
+    ManagedOrganizationalUnit = "Network"
+    SSOUserEmail              = "renatosbarretto+aws-network-hub@gmail.com"
+    SSOUserFirstName          = "Network"
+    SSOUserLastName          = "Hub"
+  }
+   
+  account_tags = {
+    "Environment" = "Production"
+    "Purpose"     = "Network Hub"
+    "ManagedBy"   = "AFT"
+    "Owner"       = "Cloud Team"
+    "Network"     = "Core"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Cloud Team"
+    change_reason       = "Creating network hub account for Transit Gateway and centralized networking"
+  }
+
+  custom_fields = {
+    group = "prod"
+    network_tier = "core"
+  }
+
+  account_customizations_name = "network-hub"
+} 
