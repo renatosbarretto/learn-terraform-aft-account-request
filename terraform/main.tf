35ecdf77018da3e04886704ad2abcca8c556bbf8
renatobarretto <renatobarretto@hotmail.com>
2025-05-06 21:43:50 -0300

    remove network

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+module "network" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "renatosbarretto+aws-network00@gmail.com"
+    AccountName               = "hub-network"
+    ManagedOrganizationalUnit = "Infrastructure"
+    SSOUserEmail              = "renatosbarretto+aws-network00@gmail.com"
+    SSOUserFirstName          = "Network"
+    SSOUserLastName           = "Team"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "dev-teams"
+    no_default_vpc = "true"
+  }
+
+  account_customizations_name = "hub-network"
+}
