825be6ed0a9918c577197d2ee24b57262857b214
renatobarretto <renatobarretto@hotmail.com>
2025-05-21 20:22:08 -0300

    new_account

---
+++ terraform/network-account.tf
@@ -0,0 +1,26 @@
+module "hub-network" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "renatosbarretto+aws-network02@gmail.com"
+    AccountName               = "hub-network-02"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "renatosbarretto+aws-network02@gmail.com"
+    SSOUserFirstName          = "Network"
+    SSOUserLastName           = "Team"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "core-network-team"
+    no_default_vpc = "true"
+  }
+  account_customizations_name = "hub-network02"
+}
